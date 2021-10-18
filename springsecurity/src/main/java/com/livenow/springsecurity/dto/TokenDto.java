2a70ded5975152e21b4f165f10ae73a5d228d97b
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-10-18 11:25:18 +0900

    dto, repository, api 구현

---
+++ springsecurity/src/main/java/com/livenow/springsecurity/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package com.livenow.springsecurity.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenDto {
+
+    private String token;
+}
